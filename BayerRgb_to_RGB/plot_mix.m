1a05c2a17ae15ac0a395decfa105d2ba30518c76
DaveCalaway <davide.gariselli@gmail.com>
2016-11-23 22:46:11 +0100

    update

---
+++ BayerRgb_to_RGB/plot_mix.m
@@ -0,0 +1,134 @@
+%% The script read npy's array ( with Bayer format ) and extract RGB's array in specific area of image.
+%  In final it plot the QE.
+% Verions 0.3 - 23-11-2016 Davide Gariselli
+%% What did you have crop?
+    %  im = readNPY('/Users/Dave/Desktop/tesi/test.npy');
+    %  J = demosaic(im,'grbg');
+    %  figure, imshow(J);
+    %  [I2, rect] = imcrop(J);
+    %  Copy position of area selected, paste it in 'Crop' value.
+clc
+clear all
+close all
+Height = 5;
+Width = 5;
+Crop = [1380.5 1098.5 24 22];
+lol = 0;
+%% Numbers of captures. 
+% If you have more then one, keep in mind to start with NDF lower to NDF highter.
+n = 1;
+
+for a=1:n
+    %% Search .npy files
+    [FileName,PathName]= uigetfile('*.npy','Select source directory:');
+    % Asks the user to select the folder where the software should look for npy log files
+
+    %Samples= input('How many samples per each flow? ')
+
+    txt_files = dir([PathName, '*.npy']);   % Search for npy files in the selected path
+    files_name = {txt_files.name};         % Name of the npy files in the folder
+    N=length (files_name);                  % How many npy files in the folder? N!
+    %% What to plot in X axis
+    vettore = strrep(files_name,'.npy','');
+    vettore = str2double(vettore);
+    %% Preallocate Memory matrix
+    color = zeros(Height*Width, 1);
+    RGB_images = zeros(3,Height*Width);
+
+    %% scroll all images
+    for i=1:N
+        %% Initialize variables.
+        filename = [PathName,txt_files(i).name];
+        %filename = strcat('/Users/Dave/Desktop/tesi/myfile/',N2str(vettore(i)),'.jpg');
+        delimiter = '\t';
+        %% Function to read NPY files,BayerRgb, into matlab.
+        BayerRgb= readNPY(filename);
+        %% BayerRgb to Bilinear Demosaicing
+        %  3 x Width-by-Height colors
+        Demo = demosaic(BayerRgb,'grbg');
+
+        %% What did you have crop?
+        I2 = imcrop(Demo, Crop);
+        imshow(I2)
+
+        %% Color extraction
+        for z=1:3
+            pos=1;
+            for x=1:Width
+                for y=1:Height
+                    color(pos) = I2(x,y,z);
+                    pos=pos+1;
+                end
+            end
+            %% Normalized and Mean
+            if z==1
+                a_B_n = color/255;
+                a_B_m = mean(a_B_n);
+                RGB_images(3,i) = a_B_m;
+            elseif z==2
+                a_G_n = color/255;
+                a_G_m = mean(a_G_n);
+                RGB_images(2,i) = a_G_m;
+            else
+                a_R_n = color/255;
+                a_R_m = mean(a_R_n);
+                RGB_images(1,i) = a_R_m;
+            end
+        end
+    end
+
+
+    %% Multi plot with NDF's mean
+
+    %% Start before first time
+    if lol == 1
+        for i=1:N
+            p = find(h_vect == vettore(i));
+            % Existing value
+            if p~=0
+                for x=1:3
+                    media = RGB_images(x,i) - h_RGB(x,p);
+                    if i == 1
+                        media_RGB(x,1) = media;
+                    else
+                        media_RGB(x,2) = media;
+                    end
+                end
+                % it is a column vector containing the mean of each row.
+                media_RGB = mean(media_RGB,2);
+            % New elements
+            else
+                q = length(h_RGB)+1;
+                for x=1:3
+                    RGB_images(x,i) = RGB_images(x,i) - media_RGB(x,1);
+                    h_RGB(x,q) = RGB_images(x,i);
+                end
+            end
+        end
+        % combine data from A and B with no repetitions. 
+        h_vect = union(h_vect,vettore);
+    end
+    
+    %% Print quantum efficiency
+    if lol == 1 || n == 1
+        if n == 1
+            h_vect = vettore;
+            h_RGB = RGB_images;
+            q=N;
+        end
+        figure()
+        grid on
+        hold on
+        for i=1:q
+            plot(h_vect(1,i),h_RGB(1,i),'r--o');
+            plot(h_vect(1,i),h_RGB(2,i),'g--o');
+            plot(h_vect(1,i),h_RGB(3,i),'b--o');
+        end
+    end
+    % Run only  the first time
+    if lol == 0 
+        h_vect = vettore;
+        h_RGB = RGB_images;
+        lol = 1;
+    end 
+end
---
+++ bayer_all.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+# disable led: https://goo.gl/b2yRoh
+# The programs run on RBp.
+# It call the folder's name and take a bayer raw.
+# After terminated, it makes a zip.
+
+from __future__ import (
+    unicode_literals,
+    absolute_import,
+    print_function,
+    division,
+    )
+
+import os
+import io
+import shutil
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+# Getch on python https://goo.gl/jeLuDv
+from getch import getch
+from time import sleep
+import picamera
+                # NDF0.5 : from 400(26) nm to 580nm(240)
+                    # NDF0.6 ISO145 -> 72
+                    #
+                # cambio filtro 581 to 591
+                # NDF0.5: from 595 to 770nm brucia
+                # cambio filtro 775 to 800nm
+# good ISO value for NDF0.5 : from 400(26) nm to 580nm(240)
+# ISO: https://goo.gl/lLJj2V
+#iso = 100
+
+def take(nome_img):
+    stream = io.BytesIO()
+    with picamera.PiCamera() as camera:
+        #camera.iso = iso
+        # Capture the image, including the Bayer data
+        camera.capture(stream, format='jpeg', bayer=True)
+
+    # Extract the raw Bayer data from the end of the stream, check the
+    # header and strip if off before converting the data into a numpy array
+
+    data = stream.getvalue()[-6404096:]
+    assert data[:4] == 'BRCM'
+    data = data[32768:]
+    data = np.fromstring(data, dtype=np.uint8)
+
+    # The data consists of 1952 rows of 3264 bytes of data. The last 8 rows
+    # of data are unused (they only exist because the actual resolution of
+    # 1944 rows is rounded up to the nearest 16). Likewise, the last 24
+    # bytes of each row are unused (why?). Here we reshape the data and
+    # strip off the unused bytes
+
+    data = data.reshape((1952, 3264))[:1944, :3240]
+
+    # Horizontally, each row consists of 2592 10-bit values. Every four
+    # bytes are the high 8-bits of four values, and the 5th byte contains
+    # the packed low 2-bits of the preceding four values. In other words,
+    # the bits of the values A, B, C, D and arranged like so:
+    #
+    #  byte 1   byte 2   byte 3   byte 4   byte 5
+    # AAAAAAAA BBBBBBBB CCCCCCCC DDDDDDDD AABBCCDD
+    #
+    # Here, we convert our data into a 16-bit array, shift all values left
+    # by 2-bits and unpack the low-order bits from every 5th byte in each
+    # row, then remove the columns containing the packed bits
+
+    data = data.astype(np.uint16) << 2
+    for byte in range(4):
+        data[:, byte::5] |= ((data[:, 4::5] >> ((4 - byte) * 2)) & 0b11)
+    data = np.delete(data, np.s_[4::5], 1)
+
+    # Now to split the data up into its red, green, and blue components. The
+    # Bayer pattern of the OV5647 sensor is BGGR. In other words the first
+    # row contains alternating green/blue elements, the second row contains
+    # alternating red/green elements, and so on as illustrated below:
+    #
+    # GBGBGBGBGBGBGB
+    # RGRGRGRGRGRGRG
+    # GBGBGBGBGBGBGB
+    # RGRGRGRGRGRGRG
+    #
+    # Please note that if you use vflip or hflip to change the orientation
+    # of the capture, you must flip the Bayer pattern accordingly
+
+    #rgb = np.zeros(data.shape + (3,), dtype=data.dtype)
+    #rgb[1::2, 0::2, 0] = data[1::2, 0::2] # Red
+    #rgb[0::2, 0::2, 1] = data[0::2, 0::2] # Green
+    #rgb[1::2, 1::2, 1] = data[1::2, 1::2] # Green
+    #rgb[0::2, 1::2, 2] = data[0::2, 1::2] # Blue
+
+    data = (data >> 2).astype(np.uint8)
+    Max = np.amax(data)
+    if Max>254:
+        #x=1
+        print('Image burned! You should use a great filter.')
+        print('Max: {}'.format(Max))
+        take(nome_img);
+    else:
+        #if x==1:
+        #    name_img = raw_input('Da dove riparto?: ')
+        #    x=0
+        print('name: {} Max: {}'.format(nome_img,Max))
+        # smallest output format: https://goo.gl/N7EeEv
+        nome_img=str(nome_img)
+        np.save(nome_img,data)
+    return
+# ----------------------------------------------
+
+
+# ----- Main's program -----
+print ('Raspberry script')
+# make a dir
+dirName = raw_input('Enter name s dir: ')
+os.makedirs(dirName)
+print ("Directory created.")
+os.chdir("/home/pi/"+dirName)
+nome_img = raw_input('Starter number: ')
+print ('SPACEBAR for shoot & ESC for terminate')
+while True:
+    key = getch()
+    key = ord(key)
+    if key == 27: #ESC
+        break
+    if key == 32: #spacebar
+        # Let the camera warm up for a couple of seconds
+        sleep(2)
+        take(nome_img);
+        nome_img=int(nome_img)
+        nome_img=nome_img+5
+        print ('scattato.')
+print ("Acquisizione completata.")
+shutil.make_archive('/home/pi/'+dirName, 'zip', '/home/pi/'+dirName)
+print ("Zipped.")
